65b3b49fcbeccd98a249f95b1daae4c93c15df8b
rosstafarian <rosstafarian1@gmail.com>
2014-02-26 01:50:13 -0600

    Display correct error message is login information is missing and there is no bootstrapped user

--- public/app/account/NavBarLoginController.js
+++ public/app/account/NavBarLoginController.js
@@ -1,14 +1,19 @@
 app.controller('NavBarLoginController', function($scope, $http, IdentityService, NotifierService, AuthService, $location) {
     $scope.identity = IdentityService;
     $scope.signin = function(username, password) {
-        AuthService.authenticateUser(username, password).then(function(success) {
-            if(success) {
-                NotifierService.notify('You have successfully signed in');
-                $location.path('/');
-            } else {
-                NotifierService.error('Username/Password combination incorrect');
-            }
-        });
+        if(!username || !password) {
+            NotifierService.error('Please enter a Username and Password');
+        }
+        else {
+            AuthService.authenticateUser(username, password).then(function(success) {
+                if(success) {
+                    NotifierService.notify('You have successfully signed in');
+                    $location.path('/');
+                } else {
+                    NotifierService.error('Username/Password combination incorrect');
+                }
+            });
+        }
     };
     $scope.signout = function() {
         AuthService.logoutUser().then(function() {
